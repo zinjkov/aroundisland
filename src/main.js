34b2c2c96910ffc0760a3b32e5c14df3333d5c0c
zinjkov <zinjkov.su@gmail.com>
2017-08-14 04:29:28 +1000

    import componetns plugin etc

--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
+import * as VueGoogleMaps from 'vue2-google-maps';
 
-Vue.config.productionTip = false
+Vue.component('icon', Icon);
+Vue.use(BootstrapVue);
+Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyCGOya76cyO2HGKPXT_b3BI4_J1JyCgSRs'
+  }
+});
+
 new Vue({
   el: '#app',
   router,
