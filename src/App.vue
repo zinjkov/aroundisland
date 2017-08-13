9ad7478c0ed8491b5f8d784578d1f22077d4f488
zinjkov <zinjkov.su@gmail.com>
2017-08-14 04:26:14 +1000

    added header footer

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,40 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+
+    <around-header></around-header>
+
     <router-view></router-view>
+
+    <around-footer></around-footer>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import AroundHeader from './components/shared/AroundHeader.vue'
+  import AroundFooter from "./components/shared/AroundFooter";
+  export default {
+    name: 'app',
+    components: {
+      AroundFooter,
+      AroundHeader
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  html, body {
+    width: 100%;
+    height: 100%;
+    padding: 0 0;
+    margin: 0 0;
+  }
+
 </style>
