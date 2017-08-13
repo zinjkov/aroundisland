f0c1d471b12020d0ceb2a54784eb1b2f545be758
zinjkov <zinjkov.su@gmail.com>
2017-08-14 04:30:06 +1000

    added tracker page

---
+++ src/components/Tracker.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="tracker">
+    <gmap-map
+      class="map"
+      :center="center"
+      :zoom="16"
+    >
+    </gmap-map>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'tracker',
+    data() {
+      return {
+        center: {lat: 43.127877, lng: 131.892663},
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .map {
+    min-height: 91vh;
+    max-width: 98.7vw;
+  }
+
+  .tracker {
+
+  }
+</style>
