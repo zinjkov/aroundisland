9e2f3c8af8ecbc8e0d7f7eedeae31d41728a8391
zinjkov <zinjkov.su@gmail.com>
2017-08-14 04:25:41 +1000

    added about team page basic html

---
+++ src/components/About.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="about">
+    <div class="container">
+      <div class="about-team">
+          <h3>как и зачем собрались вообще</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Duis vel magna a risus dictum ullamcorper. Phasellus purus metus, iaculis sed justo
+          id, dignissim scelerisque velit. In bibendum, eros sit amet cursus
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Duis vel magna a risus dictum ullamcorper. Phasellus purus metus, iaculis sed justo
+          id, dignissim scelerisque velit. In bibendum, eros sit amet cursus
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Duis vel magna a risus dictum ullamcorper. Phasellus purus metus, iaculis sed justo
+          id, dignissim scelerisque velit. In bibendum, eros sit amet cursus
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Duis vel magna a risus dictum ullamcorper. Phasellus purus metus, iaculis sed justo
+          id, dignissim scelerisque velit. In bibendum, eros sit amet cursus
+        </p>
+      </div>
+      <div class="whoiswho">
+        <div class="row">
+          <div class="col-lg-6">
+            <b-card>
+              <img src="../assets/logo.png">
+              <h4>ФИО</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Duis vel magna a risus dictum ullamcorper. Phasellus purus metus, iaculis sed justo
+                id, dignissim scelerisque velit. In bibendum, eros sit amet cursus
+              </p>
+            </b-card>
+          </div>
+          <div class="col-lg-6">
+            <b-card>
+              <img src="../assets/logo.png">
+              <h4>ФИО</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Duis vel magna a risus dictum ullamcorper. Phasellus purus metus, iaculis sed justo
+                id, dignissim scelerisque velit. In bibendum, eros sit amet cursus
+              </p>
+            </b-card>
+          </div>
+          <div class="col-lg-6">
+            <b-card>
+              <img src="../assets/logo.png">
+              <h4>ФИО</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Duis vel magna a risus dictum ullamcorper. Phasellus purus metus, iaculis sed justo
+                id, dignissim scelerisque velit. In bibendum, eros sit amet cursus
+              </p>
+            </b-card>
+          </div>
+          <div class="col-lg-6">
+            <b-card>
+              <img src="../assets/logo.png">
+              <h4>ФИО</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Duis vel magna a risus dictum ullamcorper. Phasellus purus metus, iaculis sed justo
+                id, dignissim scelerisque velit. In bibendum, eros sit amet cursus
+              </p>
+            </b-card>
+          </div>
+          <div class="col-lg-6">
+            <b-card>
+              <img src="../assets/logo.png">
+              <h4>ФИО</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Duis vel magna a risus dictum ullamcorper. Phasellus purus metus, iaculis sed justo
+                id, dignissim scelerisque velit. In bibendum, eros sit amet cursus
+              </p>
+            </b-card>
+          </div>
+          <div class="col-lg-6">
+            <b-card>
+              <img src="../assets/logo.png">
+              <h4>ФИО</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Duis vel magna a risus dictum ullamcorper. Phasellus purus metus, iaculis sed justo
+                id, dignissim scelerisque velit. In bibendum, eros sit amet cursus
+              </p>
+            </b-card>
+          </div><div class="col-lg-6">
+          <b-card>
+            <img src="../assets/logo.png">
+            <h4>ФИО</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Duis vel magna a risus dictum ullamcorper. Phasellus purus metus, iaculis sed justo
+              id, dignissim scelerisque velit. In bibendum, eros sit amet cursus
+            </p>
+          </b-card>
+        </div>
+          <div class="col-lg-6">
+            <b-card>
+              <img src="../assets/logo.png">
+              <h4>ФИО</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Duis vel magna a risus dictum ullamcorper. Phasellus purus metus, iaculis sed justo
+                id, dignissim scelerisque velit. In bibendum, eros sit amet cursus
+              </p>
+            </b-card>
+          </div>
+          <div class="col-lg-6">
+            <b-card>
+              <img src="../assets/logo.png">
+              <h4>ФИО</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Duis vel magna a risus dictum ullamcorper. Phasellus purus metus, iaculis sed justo
+                id, dignissim scelerisque velit. In bibendum, eros sit amet cursus
+              </p>
+            </b-card>
+          </div>
+          <div class="col-lg-6">
+            <b-card>
+              <img src="../assets/logo.png">
+              <h4>ФИО</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Duis vel magna a risus dictum ullamcorper. Phasellus purus metus, iaculis sed justo
+                id, dignissim scelerisque velit. In bibendum, eros sit amet cursus
+              </p>
+            </b-card>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'about'
+  }
+</script>
+
+<style scoped>
+
+</style>
