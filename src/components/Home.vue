0bd0ea81372caa7c84a36bb03913001bc6ed2548
zinjkov <zinjkov.su@gmail.com>
2017-08-14 04:28:34 +1000

    basic html css home page

---
+++ src/components/Home.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="home">
+    <div class="topic">
+      <h2>
+        ПЕРНОВ АВТОНОМНЫЙ.
+        ТЕПЕРЬ УМНЫЙ.
+      </h2>
+    </div>
+    <!--<img src="static/board1.jpg">-->
+    <div class="container">
+      <h3>
+        О Проекте
+      </h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel magna a risus dictum ullamcorper. Phasellus purus metus, iaculis sed justo id, dignissim scelerisque velit. In bibendum, eros sit amet cursus convallis, risus arcu porta lectus, non facilisis elit massa at augue. Suspendisse a varius lorem. Phasellus in hendrerit velit, vel tristique odio. Praesent dignissim lobortis sapien et tempor. Fusce purus urna, vestibulum ut eros at, laoreet placerat urna. Vivamus eu lobortis felis, in efficitur eros. Suspendisse potenti. Duis vitae justo quis est dapibus mollis. Suspendisse nisl nunc, finibus vel tempus quis, mattis quis ante. Etiam odio risus, maximus a est nec, laoreet molestie nisi. Fusce at mauris a risus venenatis aliquam eu id metus.
+
+      </p>
+
+    </div>
+    <div class=" wrapper">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4">
+            <icon class="icon" name="ship"></icon>
+            <h5>
+              Идея
+            </h5>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel magna a risus dictum ullamcorper. Phasellus purus metus, iaculis sed justo id, dignissim scelerisque velit.
+            </p>
+          </div>
+          <div class="col-lg-4">
+            <icon class="icon" name="font-awesome"></icon>
+            <h5>
+              Цели
+            </h5>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel magna a risus dictum ullamcorper. Phasellus purus metus, iaculis sed justo id, dignissim scelerisque velit.
+            </p>
+          </div>
+          <div class="col-lg-4">
+            <icon class="icon" name="cloud-upload"></icon>
+            <h5>
+              Бла-Бла
+            </h5>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel magna a risus dictum ullamcorper. Phasellus purus metus, iaculis sed justo id, dignissim scelerisque velit.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container ">
+      <b-card class="card-history ">
+        <div class="row">
+          <img class="col-lg-6" src="../../static/pernov.jpg">
+          <div class="col-lg-6">
+            <p>
+              «Пернов» — головной миноносец типа «Пернов»,
+              первый из 25 построенных для Российского императорского флота.
+            </p>
+            <p>
+              Головной миноносец «Пернов» строился силами завода А. Нормана в Гавре (Франция).
+              Спустя год после постройки миноносец передали Невскому заводу для изучения на предмет возможности серийной постройки. В 1893 были заключены контракты на постройку 24 таких кораблей. Постройка кораблей на отечественных заводах не удалась и миноносцы сильно отставали от прототипа по техническим данным. В то время как «Пернов» на испытаниях развил 25,46 узлов, некоторые корабли отечественной постройки не могли достичь и 17-узловой отметки.
+            </p>
+          </div>
+        </div>
+      </b-card>
+    </div>
+    <div class="container">
+      <h3>Партнеры?</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel magna a risus dictum ullamcorper. Phasellus purus metus, iaculis sed justo id, dignissim scelerisque velit. In bibendum, eros sit amet cursus convallis, risus arcu porta lectus, non facilisis elit massa at augue. Suspendisse a varius lorem. Phasellus in hendrerit velit, vel tristique odio. Praesent dignissim lobortis sapien et tempor. Fusce purus urna, vestibulum ut eros at, laoreet placerat urna. Vivamus eu lobortis felis, in efficitur eros. Suspendisse potenti. Duis vitae justo quis est dapibus mollis. Suspendisse nisl nunc, finibus vel tempus quis, mattis quis ante. Etiam odio risus, maximus a est nec, laoreet molestie nisi. Fusce at mauris a risus venenatis aliquam eu id metus.
+
+        Curabitur quam lacus, dapibus ac tincidunt eu, porta eu neque. Proin id nisl mi. Praesent malesuada velit leo, et ornare sapien placerat vitae. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras molestie libero erat, sed ullamcorper turpis rhoncus vel. Pellentesque nec odio dolor. Ut sed tristique eros. Nullam tortor eros, rutrum eget urna eu, facilisis lacinia nibh. Phasellus a gravida ipsum. Ut mattis varius faucibus. Integer non laoreet felis. Morbi finibus fermentum nibh. Duis congue venenatis lacus, a hendrerit velit placerat sed. Vivamus venenatis enim nec risus ornare tristique. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+
+        Morbi tincidunt, magna nec ornare elementum, augue lectus molestie elit, eget fringilla leo lacus et diam. Proin sodales vel purus nec tincidunt. Morbi sit amet nisl eget magna fermentum condimentum eu in turpis. Morbi commodo quam ac quam euismod varius sed nec leo. Integer vitae quam lobortis, facilisis ante sit amet, cursus tellus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse id nisi condimentum, auctor dui id, ultricies eros. Donec eu dapibus nisi, in dapibus sem. Quisque velit ligula, convallis eget sem nec, bibendum lobortis enim.
+
+        Aliquam pretium libero massa. Duis vel quam porta, tempus leo vitae, condimentum dui. Pellentesque sit amet elementum ipsum, quis aliquet ipsum. Donec vitae sapien lorem. Nunc augue odio, vehicula vel facilisis in, finibus hendrerit mi. Mauris porta a libero sit amet sodales. Donec cursus mollis mauris vitae tristique. Phasellus sit amet sollicitudin nisl. Nullam laoreet ut sem non mattis. Sed tempus lacus vel odio porta gravida. Aenean blandit facilisis ipsum, sed luctus sapien fringilla ut. Curabitur ullamcorper mi ut nisi commodo, in condimentum orci euismod.
+
+        Praesent quis ante id mauris congue blandit vitae non nibh. Sed facilisis eleifend neque non efficitur. Pellentesque fermentum consectetur felis sed dictum. Donec sed ornare nisl. Donec gravida volutpat dui. Etiam non purus arcu. Proin malesuada risus diam, et cursus orci tempus ut. Nullam condimentum quis urna eu volutpat. Nulla dapibus ipsum ipsum, eget feugiat tellus consequat ege</p>
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+  import Icon from "../../node_modules/vue-awesome/components/Icon";
+
+  export default {
+    components: {Icon},
+    name: 'home'
+  }
+</script>
+
+<style scoped>
+  .icon {
+    height: 30px;
+    width: 30px;
+  }
+
+  .topic {
+    background-image: url(../../static/board1.jpg);
+    min-height: 311px;
+  }
+
+  .wrapper {
+    padding: 20px 0;
+    background-color: #00365f;
+    color: white;
+  }
+
+  h2 {
+    padding-top: 100px;
+    color: white;
+    font-weight: 200;
+  }
+
+  .pernov_img {
+    flex: 1;
+    max-width: 100px;
+  }
+
+  .card-history {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .card-history-text {
+    flex: 2;
+  }
+
+  .gray {
+    background-color: gray;
+
+  }
+</style>
