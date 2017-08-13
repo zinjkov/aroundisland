3f9f071ae11cfe292d8c33d8de968c3c2b03df60
zinjkov <zinjkov.su@gmail.com>
2017-08-14 04:28:51 +1000

    updates routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Home from '@/components/Home'
+import Gallery from '@/components/Gallery'
+import About from '@/components/About'
+import Tracker from '@/components/Tracker'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
-    }
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
+    },
+    {
+      path: '/gallery',
+      name: 'gallery',
+      component: Gallery,
+    },
+    {
+      path: '/tracker',
+      name: 'tracker',
+      component: Tracker
+    },
   ]
 })
