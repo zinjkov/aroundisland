bc1645439c83bdc80cf963ef676b7a72b180e184
zinjkov <zinjkov.su@gmail.com>
2017-08-14 04:26:40 +1000

    footer component basic css html

---
+++ src/components/shared/AroundFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-navbar class="aroundFooter" toggleable type="light" variant="link" toggle-breakpoint="md">
+
+  </b-navbar>
+</template>
+
+<script>
+
+  export default {
+    name: 'around-footer'
+  }
+</script>
+
+<style scoped>
+  .aroundFooter {
+    background-color: #00365f;
+    color: white;
+    height: 75px;
+  }
+
+  span {
+    color: white
+  }
+</style>
