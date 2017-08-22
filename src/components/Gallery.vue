a9f4303119d634aa36c4aa94ad2a9a8bc0079641
zinjkov <zinjkov.su@gmail.com>
2017-08-22 12:59:12 +1000

    added request board photo

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,32 +1,62 @@
 <template>
-  <div class="gallery">
-    <div class="container">
-      <div class="row">
-        <b-card class="col-lg-4">
-          <img src="../../static/board2.jpg">
-          <div class="card-view">
-            <b-button variant="primary">С лодки</b-button>
-          </div>
-        </b-card>
-        <b-card class="col-lg-4"><img src="../../static/board2.jpg">
-          <div class="card-view">
-            <b-button variant="primary">СМИ</b-button>
-          </div>
-        </b-card>
-        <b-card class="col-lg-4"><img src="../../static/board2.jpg">
-          <div class="card-view">
-            <b-button variant="primary">After-Party</b-button>
-          </div>
-        </b-card>
+  <div>
+    <div class="gallery">
+      <div class="container">
+        <div class="row">
+          <b-card class="col-lg-4">
+            <img :src=src_img>
+            <div class="card-view">
+              <b-button variant="primary">С лодки</b-button>
+            </div>
+          </b-card>
+          <b-card class="col-lg-4"><img src="../../static/board2.jpg">
+            <div class="card-view">
+              <b-button variant="primary">СМИ</b-button>
+            </div>
+          </b-card>
+          <b-card class="col-lg-4"><img src="../../static/board2.jpg">
+            <div class="card-view">
+              <b-button variant="primary">After-Party</b-button>
+            </div>
+          </b-card>
+        </div>
       </div>
     </div>
+    <div class="container">
+      <h3>FROM BOARD</h3>
+      <image-list :imageList=image_list></image-list>
+    </div>
   </div>
+
 </template>
 
 <script>
-
+  import {mapGetters} from 'vuex'
+  import ImageList from './shared/ImageList'
   export default {
-    name: 'gallery'
+    name: 'gallery',
+    components: {
+      ImageList
+    },
+    data() {
+      return {
+        src_img: ""
+      }
+    },
+    mounted() {
+      const domain = 'http://127.0.0.1:8000/';
+//      const domain = 'http://www.aroundisland.ru/';
+      this.src_img = 'http://127.0.0.1:8000/' + this.last_img;
+    },
+    computed: {
+      ...mapGetters(
+        [
+          'last_img',
+          'image_list'
+        ]
+      )
+    }
+
   }
 </script>
 
